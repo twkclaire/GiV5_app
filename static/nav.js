6746bca365e6931e9185d4066cb334023c6dadf5
Claire Kao <163644958+twkclaire@users.noreply.github.com>
2024-08-22 15:37:26 +0200

    drop down button file and update logo

---
+++ static/nav.js
@@ -0,0 +1,50 @@
+var userId=""
+window.onload = function checkSigninStatus() {
+    const token = localStorage.getItem("token");
+    let content="";
+    const dropdownContent=document.querySelector(".dropdown-content");
+  
+    if (token) {
+      fetch("/api/user/auth", {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      })
+        .then((resp) => {
+          if (!resp.ok) {
+            return resp.json().then((data) => {
+              throw new Error(`HTTP error! Status: ${resp.status}, Message: ${data.detail}`);
+            });
+          }
+          return resp.json();
+        })
+        .then((data) => {
+          console.log("Fetch successful:", data);
+          document.dispatchEvent(new CustomEvent('userSignedIn', { detail: data.data.name }));
+          userId=data.data.id
+          console.log("is this the user id:",userId)
+          content +=`
+                <a href="/member/${userId}"">My Page</a>
+                <a href="#" onclick="deleteToken(); return false;">Log out</a>
+          `  
+          dropdownContent.innerHTML += content;
+        })
+        .catch((err) => {
+          console.error("Error:", err.message);
+        });
+    } else {
+      console.error("Token not found");
+      content +=`
+                <a href="/signin">Sign in</a>
+                <a href="/register">Sign up</a>
+
+          `  
+          dropdownContent.innerHTML += content;
+    }
+  };
+
+  function deleteToken() {
+    let token = localStorage.removeItem("token");
+    console.log("user signed out");
+    window.location.reload();
+    return token;
+  }
